6a6d43cc879c1e882895bfb2669aff9fb148d610
vix <churink9@gmail.com>
2024-04-05 16:44:07 +0300

    first commit

---
+++ 1. Простое исключение.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct bad_length
+{
+	int forbidden_length;
+	bad_length(int length) :forbidden_length(length) {}
+
+};
+
+int function(std::string str, int forbidden_length)
+{
+	if (str.length() == forbidden_length)
+	{
+		throw bad_length(forbidden_length);
+	}
+
+	return str.length();
+}
+
+
+int main()
+{
+	int forbidden_length;
+
+	cout << "Введите запретную длину строки: ";
+	cin >> forbidden_length;
+
+	string str;
+	while (true)
+	{
+		cout << "Введите слово: ";
+		cin >> str;
+
+		try
+		{
+			int length = function(str, forbidden_length);
+			cout << "Длина слова " << '"' << str << '"' << " равна " << length << endl;
+
+		}
+		catch (bad_length& bl)
+		{
+			cout << "Вы ввели слово с запретной длины! До свидания ";
+			return 0;
+		}
+	}
+	return 0;
+}
+
